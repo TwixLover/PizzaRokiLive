06c42fd7115ea424b3b83d8516a0331d848515cb
TwixLover <129199746+TwixLover@users.noreply.github.com>
2026-06-23 19:46:54 +0200

    Login fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -356,7 +356,8 @@ router.post("/logout", (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     sameSite: "lax",
-    secure: false,
+    secure: true,
+    sameSite: "none",
   });
   res.json({ message: "Logged out" });
 });
