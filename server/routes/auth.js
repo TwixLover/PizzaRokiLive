4a54a09a2d7106aae4b92d1f679cd1b43f55baae
TwixLover <129199746+TwixLover@users.noreply.github.com>
2026-07-02 01:02:22 +0200

    Email verification 1.5

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -16,13 +16,17 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  family: 4,          // <-- IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   tls: {
     rejectUnauthorized: false,
+  },
+  getSocket: undefined,
+  dnsTimeout: 10000,
+  lookup: (hostname, options, callback) => {
+    dns.lookup(hostname, { family: 4 }, callback);
   },
 });
 transporter.verify((err, success) => {
