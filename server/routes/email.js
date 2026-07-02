bd34d7cdb7a30607d0b18a979d799c9df80f9f49
TwixLover <129199746+TwixLover@users.noreply.github.com>
2026-07-03 00:37:33 +0200

    Brevo 1.4

--- server/routes/email.js
+++ server/routes/email.js
@@ -1,39 +1,47 @@
-import * as Brevo from "@getbrevo/brevo";
-
-const api = new Brevo.TransactionalEmailsApi();
-
-api.setApiKey(
-  Brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
-);
 export async function sendEmail({
   to,
   name,
   subject,
   html,
 }) {
-  try {
-    await api.sendTransacEmail({
+  const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+    method: "POST",
+    headers: {
+      "accept": "application/json",
+      "content-type": "application/json",
+      "api-key": process.env.BREVO_API_KEY,
+    },
+    body: JSON.stringify({
       sender: {
         name: "Pizza Roki",
-        email: "trashcanforzs@gmail.com",
+        email: process.env.BREVO_SENDER,
       },
 
       to: [
         {
           email: to,
-          name,
+          name: name,
         },
       ],
 
-      subject,
+      subject: subject,
 
       htmlContent: html,
-    });
+    }),
+  });
 
-    console.log("Email sent");
-  } catch (err) {
-    console.error(err);
-    throw err;
+  if (!response.ok) {
+    const errorText = await response.text();
+
+    console.error("BREVO ERROR:");
+    console.error(errorText);
+
+    throw new Error(errorText);
   }
+
+  const data = await response.json();
+
+  console.log("Email sent:", data);
+
+  return data;
 }
